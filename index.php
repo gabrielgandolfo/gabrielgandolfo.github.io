42dd4826c90108d8a3745dd18c4ccab132da004e
Gabriel Gandolfo <gabriel.gandolfo@gmail.com>
2016-07-06 03:49:10 -0300

    add files

---
+++ index.php
@@ -0,0 +1,265 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+<head>
+     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+	<meta name="description" content="Sou programador há mais de 3 anos. Especializado em php, javascript, ios." />
+	<meta name="keywords" content="programador, programador ios, programador php, programador php osasco, programador osasco, programador iphone, ios developer, php developer." />
+	<meta name="language" content="pt-br" />
+	<meta name="copyright"content="Gabriel Silva Gandolfo">
+	<meta name="author" content="Gabriel Silva Gandolfo, gabriel@gabrielgandolfo.com" />
+	<meta name="url" content="http://www.gabrielgandolfo.com" />
+	<meta name="abstract" content="Programador iOS e PHP." />
+	<meta name="alexa" content="100" />
+	<meta name="pagerank" content="10" />
+	<meta name="revisit" content="2 days" />
+	<meta name="revisit-after" content="2 days" />
+	<meta name="robots" content="all, index, follow" />
+	<meta name="coverage" content="Worldwide">
+	<meta name="distribution" content="Global">
+	<meta name="rating" content="General">
+
+	<script type="text/javascript">
+	
+	  var _gaq = _gaq || [];
+	  _gaq.push(['_setAccount', 'UA-20171906-2']);
+	  _gaq.push(['_trackPageview']);
+	
+	  (function() {
+	    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	  })();
+	
+	</script>
+	
+     <link href='http://fonts.googleapis.com/css?family=Sanchez' rel='stylesheet' type='text/css'>
+     <link href='http://fonts.googleapis.com/css?family=Rokkitt' rel='stylesheet' type='text/css'>
+     <link rel="shortcut icon" href="images/coffee.ico" />
+
+     <title>Programador Gabriel Silva Gandolfo</title>
+
+     <style type="text/css">
+/*      font-family: 'Sanchez', serif; */
+        * { margin: 0; padding: 0; }
+        body { font: 16px Helvetica, Sans-Serif; line-height: 24px; background: url(images/noise.jpg); }
+        .clear { clear: both;}
+        .row {border:solid 1px;}
+        #page-wrap { width: 800px; margin: 40px auto 60px; }
+        #pic { float: right; margin: -30px 0 0 0; }
+        h1 { font-family:'Sanchez', serif;margin: 0 0 16px 0; padding: 0 0 16px 0; font-size: 42px; font-weight: bold; letter-spacing: -2px; border-bottom: 1px solid #999; width:80%; }
+        h1 span { font-style: italic; font-family: 'Sanchez', Serif; font-size: 18px; color: #999; font-weight: normal; }
+        h2 { font-size: 20px; margin: 0 0 6px 0; position: relative;}
+        h2 span { position: absolute; bottom: 0; right: 0; font-style: italic; font-family: Georgia, Serif; font-size: 16px; color: #999; font-weight: normal; }
+        p { margin: 0 0 16px 0; }
+        a { color: #999; text-decoration: none; border-bottom: 1px dotted #999; }
+        a:hover { border-bottom-style: solid; color: black; }
+        ul { margin: 0 0 32px 17px; }
+        #objective { width: 500px; float: left; }
+        #objective p { font-family: 'Sanchez', Serif; font-style: italic; color: #666; }
+        dt { font-style: italic; font-weight: bold; font-size: 18px; text-align: right; padding: 0 26px 0 0; width: 150px; float: left; height: auto; border-right: 1px solid #999;  }
+        
+        dd { width: 600px; float: right; }
+        dd.clear { float: none; margin: 0; height: 15px; }
+     </style>
+</head>
+
+<body>
+
+    <div id="page-wrap">
+    
+        <img src="images/profile.png" alt="Profile" id="pic" />
+    
+        <div id="contact-info" class="vcard">
+        
+            <!-- Microformats! -->
+        
+            <h1 class="fn">Gabriel Silva Gandolf <span>Developer</span></h1>
+        
+            <p>
+                Celular: <span class="tel">(11) 98070-0468</span><br />
+                Email: <a class="email" href="mailto:gabriel@gabrielgandolfo.com">gabriel@gabrielgandolfo.com</a><br />
+                C.V DOC: <a href="./cv_gabrielgandolfo.doc" target="_BLANK">Download</a><br />
+<!--                 C.V PDF: <a href="cv_gabrielgandolfo.pdf" target="_BLANK">Download</a>  -->
+            </p>
+        </div>
+                
+        <div id="objective">
+            <p>
+                 
+            </p>
+        </div>
+        
+        <div class="clear"></div>
+        
+        <dl>
+            <dd class="clear"></dd>
+            
+            <dt>Educação</dt>
+            <dd class="educacao">
+                <h2>Quaddro - Treinamentos em iOS</h2>
+                <p>
+	                <ul>
+	                	<li>Formação iOS Programmer - 220 Horas</li>
+	                	<li>Curso Design de Interface Mobile - 60 Horas</li>
+	                </ul>                
+                </p>
+                   
+                   
+                <h2>Impacta Certificação e Treinamento</h2>
+                <p>
+					<ul>
+						<li>PHP Módulos I e II - 80 Horas</li>
+						<li>MySQL Módulos I e II - 80 Horas</li>
+						<li>JavaScript - 40 Horas</li>
+						<li>jQuery - 40 Horas</li>
+					</ul>
+				</p>
+				<h2>MestreSEO (Setembro 2013)</h2>
+                <p>
+					<ul>
+						<li>Curso de SEO</li>
+						<li>Curso de Analytics</li>
+					</ul>
+				</p>   
+            </dd>
+            
+            <dd class="clear"><div class="row"></div></dd>
+            
+            <dt>Qualificações</dt>
+            <dd>
+                <h2>Programação</h2>
+                <p>
+					<ul>
+						<li>PHP
+							<ul>					
+								<li>Frameworks
+									<ul>
+										<li>Laravel Framework</li>
+										<li>CodeIgniter Framework</li>
+										<li>CakePHP Framework</li>
+									</ul>									
+								</li>	
+								<li>
+									phpDocumentor
+								</li>							
+							</ul>
+						</li>
+						<li>Objective-C
+							<ul>
+								<li>Cocoa</li>
+								<li>Cocoa Touch</li>
+							</ul>
+						</li>
+						<li>JavaScript
+							<ul>
+								<li>jQuery</li>
+								<li>JSON</li>
+								<li>Ajax</li>								
+							</ul>
+						</li>
+						<li>HTML/CSS
+							<ul>
+								<li>HTML 5</li>
+								<li>CSS 3</li>
+								<li>SEO</li>								
+							</ul>
+						</li>						
+					</ul>
+				</p>
+					
+				<h2>Banco de dados</h2>	
+				<p>
+					<ul>
+						<li>MySQL</li>
+						<li>PostgreSQL</li>
+						<li>Oracle</li>
+					</ul>
+				</p>
+				
+				<h2>S.O</h2>
+				<p>
+					<ul>
+						<li>Linux - Ubuntu/Debian/RedHat</li>
+						<li>MacOSX</li>
+						<li>Windows</li>
+					</ul>
+				</p>
+            </dd>
+            
+            <dd class="clear"><div class="row"></div></dd>
+            
+            <dt>Experiência</dt>
+            <dd>
+            	<h2>4Kode<span>2014-atualmente</span></h2>
+                <ul>
+                    <li>Dev
+						<ul>
+							<li>PHP - Laravel 4 / Laravel 5 / CodeIgniter 3</li>
+							<li>jQuery</li>
+							<li>Ajax</li>
+							<li>Json</li>
+							<li>MySQL</li>
+							<li>Aplicativos iOS</li>
+						</ul>
+                    </li>
+                </ul>
+                
+            	<h2>Dainet<span>2013-2014</span></h2>
+                <ul>
+                    <li>Programador
+						<ul>
+							<li>PHP - WhalePHP</li>
+							<li>jQuery/Ajax</li>
+							<li>MySQL</li>
+							<li>iOS</li>
+						</ul>
+                    </li>
+                </ul>
+
+				<h2>Vendedoor Geração de Leads<span>2011-2013</span></h2>
+                <ul>
+                    <li>Programador
+						<ul>
+							<li>PHP - CodeIgniter/Laravel</li>
+							<li>jQuery/Ajax</li>
+							<li>MySQL/PostgreSQL</li>
+						</ul>
+                    </li>
+                </ul>
+                
+                <h2>InterSolution - Soluções em Internet <span>Jan - Jul 2011</span></h2>
+                <ul>
+                    <li>Programador 
+						<ul>
+							<li>PHP (HTML/CSS)</li>
+							<li>MySQL</li>
+						</ul>
+                    </li>
+                </ul> 
+                
+                 <h2>Sindicato dos professores de Osasco <span>2010-2011</span></h2>
+                <ul>
+                    <li>Suporte Técnico</li>
+                    <li>Programador PHP</li>
+                </ul>
+                
+            </dd>
+            
+            <dd class="clear"><div class="row"></div></dd>
+             
+            <dt>Copyright © 2011-2016 </dt>
+            <dd>Gabriel Silva Gandolfo</dd>
+                        
+            <dd class="clear"></dd>
+        </dl>
+        
+        <div class="clear"></div>
+    
+    </div>
+
+</body>
+
+</html>
